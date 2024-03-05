52ddd8ac4c0edb69b63293d372f0f2c6bdf91d7a
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-03-05 23:39:54 +0700

    add detail page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,9 +4,11 @@ import Footer from '../parts/Footer'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
 import Sitemap from '../parts/HomePage/Sitemap'
-import ProductDetails from './Details/ProductDetails'
+// import ProductDetails from './Details/ProductDetails'
 import { withRouter } from 'react-router'
-import Suggestion from './Details/Suggestion'
+// import Suggestion from './Details/Suggestion'
+import Suggestion from '../parts/Details/Suggestion'
+import ProductDetails from '../parts/Details/ProductDetails'
 
 function Details() {
   return (
